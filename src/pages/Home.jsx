872d95131c96bbcacb15a65a83dc9eb2ddb82c67
Hi Tran <thuchitran@gmail.com>
2024-07-30 21:45:45 +1000

    update homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Home = () => {
-  return <div>Updated Home Page!</div>;
+  const [base, setBase] = useState();
+  const [exponent, setExponent] = useState();
+
+  const inputChangeHandler = (e, type) => {
+    if (type === "base") {
+      setBase(e.target.value);
+    } else if (type === "exponent") {
+      setExponent(e.target.value);
+    }
+  };
+
+  const formSubmitHandler = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      "https://s7uwt99beh.execute-api.ap-southeast-2.amazonaws.com/dev",
+      {
+        base: base,
+        exponent: exponent,
+      }
+    );
+    console.log(response);
+  };
+
+  const callAPI = (base, exponent) => {};
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <label>Base Number:</label>
+      <input
+        type='text'
+        id='base'
+        onChange={(e) => inputChangeHandler(e, "base")}
+      />
+      <label>Power Number: </label>
+      <input
+        type='text'
+        id='exponent'
+        onChange={(e) => inputChangeHandler(e, "exponent")}
+      />
+      <button type='submit'>Calculate</button>
+    </form>
+  );
 };
 
 export default Home;
